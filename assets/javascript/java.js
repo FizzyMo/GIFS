d72c47d7a832e7b4ab51ab3ac1561dcc1583c3da
FizzyMo <carisasaenz@gmail.com>
2019-03-09 12:25:44 -0700

    I give up. Its staying the way it is

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,26 +1,25 @@
-var addArray = ["Lana Del Rey", "Dogs", "Birds", "Snow", "Ice Cream"];
+var athletesArray = ["Lana Del Rey", "Dogs", "Birds", "Snow", "Ice Cream"];
 
 $(document).ready(function () {
-    for (var i = 0; i < addArray.length; i++) {
-        $("#athl-buttons").append("<button type='button' onclick='searchGif(\"" + 
-        adArray[i] + "\")' class='btn btn-primary' value=' "
-            + addArray[i] + "'> " + addArray[i] + " </button>");
+    for (var i = 0; i < athletesArray.length; i++) {
+        $("#athlete-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' "
+            + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
     }
 });
 
 function athleteButtonClicked() {
-    var userInput = $('#athl-input').val();
+    var userInput = $('#athlete-input').val();
     userInput.preventDefault();
     searchGif(userInput);
 }
 
 
 function submitButtonClicked() {
-    var userInput = $('#athl-input').val();
+    var userInput = $('#athlete-input').val();
 
 
     if (userInput) {
-        $('#athl-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " +
+        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " +
             userInput + "'> " + userInput + " </button>");
     }
 }
@@ -36,16 +35,15 @@ function searchGif(gifName) {
 }
 
 function displayGif(response) {
-    $('#athl').empty();
+    $('#athletes').empty();
     for (var i = 0; i < response.data.length; i++) {
         var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
         var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
             '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url +
-             '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
 
         image = '<div class="col-md-4">' + image + "</div>";
-        $('#athl').append(image);
+        $('#athletes').append(image);
     }
 
     $('.movImage').on('click', function () {
