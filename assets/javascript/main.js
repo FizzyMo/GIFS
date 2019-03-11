a3087e9364502607f8ff03dec351e02d6abe6559
FizzyMo <carisa1000@yahoo.com>
2019-03-10 21:40:47 -0700

    finally changed it

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,25 +1,25 @@
-var athletesArray = ["Lana Del Rey", "Dogs", "Birds", "Snow", "Ice Cream"];
+var ranArray = ["Lana Del Rey", "Dogs", "Birds", "Snow", "Ice Cream"];
 
 $(document).ready(function () {
-    for (var i = 0; i < athletesArray.length; i++) {
-        $("#athlete-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' "
-            + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
+    for (var i = 0; i < ranArray.length; i++) {
+        $("#rando-buttons").append("<button type='button' onclick='searchGif(\"" + ranArray[i] + "\")' class='btn btn-primary' value=' "
+            + ranArray[i] + "'> " + ranArray[i] + " </button>");
     }
 });
 
-function athleteButtonClicked() {
-    var userInput = $('#athlete-input').val();
+function randoButtonClicked() {
+    var userInput = $('#rando-input').val();
     userInput.preventDefault();
     searchGif(userInput);
 }
 
 
 function submitButtonClicked() {
-    var userInput = $('#athlete-input').val();
+    var userInput = $('#rando-input').val();
 
 
     if (userInput) {
-        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " +
+        $('#rando-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " +
             userInput + "'> " + userInput + " </button>");
     }
 }
@@ -35,7 +35,7 @@ function searchGif(gifName) {
 }
 
 function displayGif(response) {
-    $('#athletes').empty();
+    $('#random').empty();
     for (var i = 0; i < response.data.length; i++) {
         var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
         var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
@@ -43,7 +43,7 @@ function displayGif(response) {
             ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
 
         image = '<div class="col-md-4">' + image + "</div>";
-        $('#athletes').append(image);
+        $('#random').append(image);
     }
 
     $('.movImage').on('click', function () {
